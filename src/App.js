17948ef29506a7ae392ce752b873d2abdb7d090b
Kenneth Liou <90430079+kungfu-k3nny@users.noreply.github.com>
2024-03-14 20:20:36 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,31 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [pr, setPrice] = useState(0)
+  const addPrice = (addedNum) => { setPrice(pr + addedNum) }
+  const [cartList, setCartList] = useState([])
+  const addItem = (itemName) => { setCartList([...cartList, itemName])}
+  
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <div>
+          <BakeryItem name={item.name} image={item.image} description={item.description} price={item.price} totalPrice={addPrice} totalItems={addItem}/>
+          <br></br>
+        </div>
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+
+        {cartList.map((item, index) => (
+          <div> {item} </div>
+        ))}
+
+        <p>Total price: {pr}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,18 @@
-// TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    const twoFuncs = () => {
+        props.totalPrice(props.price);
+        props.totalItems(props.name);
+    }
+    
+    return (
+        <div>
+            <hstack>
+                <div>{props.name}</div>
+                <img src={props.image} />
+                <div>{props.description}</div>
+                <div>{props.price}</div>
+                <button onClick={() => twoFuncs()}> Add to cart </button>
+            </hstack>
+        </div>
+    );
+}
